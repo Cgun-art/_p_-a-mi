0d9bd924fc81454b0a7d3378061df3d83da09fe9
Cgun-art <migueloproio903@gmail.com>
2025-11-20 18:05:26 -0300

    Update pigs.cc

--- 🕴🏼/pigs.cc
+++ 🕴🏼/pigs.cc
@@ -1,1 +1,3 @@
- 
+#include <iostream>
+#include <string>
+int main(){std::string word="string";word+="ing";std::cout<<word<<std::endl;int result=6*80+80;std::cout<<"Result: "<<result<<std::endl;return 0;} 
